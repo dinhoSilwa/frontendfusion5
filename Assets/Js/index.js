ed119927e4fab5e27f938950750e62328830957f
Carlos Bruno <bruneco8@gmail.com>
2024-02-24 14:12:13 -0300

    add função destaque mosaico

--- Assets/Js/index.js
+++ Assets/Js/index.js
@@ -1,1 +1,22 @@
-/* Javascript */
+const AllImagenMosaico = document.querySelectorAll(".mosaico_box-imgs")
+
+AllImagenMosaico.forEach((Select) => {
+     Select.addEventListener("mouseenter", () => {
+        Select.classList.toggle("box-select")
+    }) 
+        Select.addEventListener("mouseleave", () => {
+            Select.classList.toggle("box-select")
+        })
+    AllImagenMosaico.forEach((NoSelect) => {
+        if(NoSelect !== Select) {
+            Select.addEventListener("mouseenter",() => {
+                NoSelect.classList.toggle("box-noselect")
+                
+
+            })
+            Select.addEventListener("mouseleave", () => {
+                NoSelect.classList.toggle("box-noselect")
+            } ) 
+        }
+    })
+});
